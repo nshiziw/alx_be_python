b1dfe8327ab03d9aff3cf80b353d19061680eaf7
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-13 15:30:59 +0200

    developmne

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+add = number1 + number2
+minus = number2 - number1
+times = number1 * number2
+
+print(f"Addition of 10 and 5 is: {add}")
+print(f"Subtraction of 10 and 5 is: {minus}")
+print(f"Multiplication of 10 and 5 is: {times}")
