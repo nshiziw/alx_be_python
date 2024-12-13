43ecbd6bc72bb7b60e1894a9525b0d31bd7ea0db
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-13 15:37:29 +0200

    developmne

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,3 @@
+Age = input("How old are you?")
+futureAge = int(Age) + 27
+print(f"In 2050, you will be {futureAge} years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,3 @@
+hours = 2
+Seconds = hours * 3600
+print(f"{hours} hour(s) is {Seconds} seconds.")
