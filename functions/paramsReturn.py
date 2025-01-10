08235298e30f2f36a90a5d7adfc8e8e397c22678
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-10 21:20:26 +0200

    development

--- functions/paramsReturn.py
+++ functions/paramsReturn.py
@@ -11,3 +11,11 @@ def triangle_area(base, height):
 b=40
 h=35
 print(f"The are of a triangle whose base is {b} and height is {h} is {triangle_area(b, h)}")
+
+def user_info(name, age=None):
+    """Prints user information """
+    print(f"User's name is: {name}")
+    if age:
+        print(f"User's age is: {age}")
+user_info(name="Wilson")
+user_info(name="Patrick", age=45)
