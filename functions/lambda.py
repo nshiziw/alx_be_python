687ff8808c12c7c64bd56772026ae31c59a0ecc1
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-10 21:03:23 +0200

    development

---
+++ functions/lambda.py
@@ -0,0 +1,8 @@
+# lambda function to create area of square
+area_square = lambda s: s*s
+print (f"The area of square whose side is 5 cm is {area_square(5)} cm^2")
+
+# regular function to create area of square
+def areaSquare(s):
+    return s* s
+print (f"The area of square whose side is 5 cm is {areaSquare(5)} cm^2")
