5a447902e38c5d7c2ad3bae11904d9486650a01f
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-12 15:39:36 +0200

    development

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,8 +1,5 @@
 def display_menu():
-    """
-    Display the menu options to the user.
-    """
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
