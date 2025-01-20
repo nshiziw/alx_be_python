f6b489b1f02cabf9bd45306845556e4fba2fe171
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-20 19:56:59 +0200

    development

--- free_code_camp/encrypt_text.py
+++ free_code_camp/encrypt_text.py
@@ -1,19 +1,28 @@
 text = 'Hello Zaira'
-shift = 3
+custom_key = 'python'
 
-def caesar(message, offset):
+def vigenere(message, key):
+    key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
     for char in message.lower():
+    
+        # Append space to the message
         if char == ' ':
             encrypted_text += char
-        else:
+        else:        
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
-
-caesar(text, shift)
-caesar(text, 13)
+    
+    return encrypted_text
+    
+encryption = vigenere(text, custom_key)
+print(encryption)
