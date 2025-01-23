84eb3d93703ffe7af96e8926d5966931f90ef1f5
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-23 21:17:28 +0200

    fixing error on __del__

--- oop/book_class.py
+++ oop/book_class.py
@@ -11,5 +11,5 @@ class Book:
         return f"Book('{self.title}', '{self.author}', {self.year})"
     
     def __del__(self):
-        print (f"Deleting ({self.title})")
+        print (f"Deleting {self.title}")
     
