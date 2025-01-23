d8219f0ac221ba871bac8d48ab48685e7da25c73
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-23 19:27:23 +0200

    development

---
+++ OOP/init.py
@@ -0,0 +1,8 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# create a point object
+point = Point(3, 5)
+print(f"Point coordinates: ({point.x}, {point.y})")
