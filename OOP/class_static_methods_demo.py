fdffb329595139dfcbfd4a258557fc393b0f2c24
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-23 20:35:50 +0200

    Distinguishing Between Class Methods and Static Methods

---
+++ OOP/class_static_methods_demo.py
@@ -0,0 +1,19 @@
+class Calculator:
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        """
+        Static method to return the sum of two numbers.
+        """
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        """
+        Class method to return the product of two numbers.
+        Prints the class attribute 'calculation_type' before calculating.
+        """
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
--- OOP/main.py
+++ OOP/main.py
@@ -2,6 +2,7 @@ from book_class import Book
 from library_system import Book, EBook, PrintBook, Library
 from polymorphism_demo import Shape, Rectangle, Circle
 import math
+from class_static_methods_demo import Calculator
 
 def main():
     # Creating an instance of Book
@@ -42,6 +43,16 @@ def main():
 
     for shape in shapes:
         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+
+
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
 
 if __name__ == "__main__":
     main()
