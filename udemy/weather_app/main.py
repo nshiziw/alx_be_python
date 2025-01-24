0f2c4e7f2c4f638f3ec80d4ed15c36a528e7dc79
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-24 19:32:39 +0200

    development

---
+++ udemy/weather_app/main.py
@@ -0,0 +1,20 @@
+from typing import Union 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_items():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+
+    return HTMLResponse(content=html_content, status_code=200)
